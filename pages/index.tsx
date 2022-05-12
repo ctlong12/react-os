7d6a3fc0d12fde15c4db510d8c34130db2c38bec
Chandler Long <admin@sustainapp.com>
2022-05-12 15:52:11 -0400

    Eslint, stylelint, and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-//import styles from '../styles/Home.module.css'
+// import Head from 'next/head';// import Image from 'next/image';
+// import styles from '../styles/Home.module.css'
+import type { ReactElement } from 'react';
 
-export default function Home() {
-  const message = 'Hello, World!'
-  const element = <h1>{ message }</h1>
-  return element
+export default function Home(): ReactElement {
+  const message = 'Hello, World!';
+  const element = <h1>{message}</h1>;
+  return element;
 }
